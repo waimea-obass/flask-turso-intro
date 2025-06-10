776ec67f1d92aa0f1bd59f42b05c4dd629f54d4f
Ollie <obass@waimea.school.nz>
2025-06-10 13:26:11 +1200

    Did the add, need to do cdelete

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask          import Flask
 from flask          import render_template
 from flask          import redirect
+from flask          import request
 from libsql_client  import create_client_sync
 from dotenv         import load_dotenv
 import os
@@ -69,12 +70,34 @@ def show_thing(id):
 def new_thing():
     return render_template("pages/thing-form.jinja")
 
-
+#-------------------------------------------------------
+#Process a new thing
+#-----------------------------------------------------------
+@app.post("/add-thing")
+def add_thing():
+    name = request.form.get("name")
+    price = request.form.get("price")
+    client = connect_db()
+    sql = """
+        INSERT INTO things (name, price)
+        VALUES (?, ?)
+    """
+    values = [name, price]
+    client.execute(sql, values)
+    return redirect("/") 
 #-----------------------------------------------------------
 # Thing deletion
 #-----------------------------------------------------------
 @app.get("/delete/<int:id>")
 def delete_thing(id):
+
+    client = connect_db()
+    sql = """
+        DELETE FROM things
+        WHERE id=?
+    """
+    values = [id]
+    client.execute(sql, values)
     return redirect("/")
 
 
